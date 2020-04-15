7378bee8b7a5158c322637358633149cdbd8548d
dylan-losey <losey@vt.edu>
2020-04-14 21:59:25 -0500

    playing with real humans

---
+++ april-21/models.py
@@ -0,0 +1,116 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import pickle
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Convention(nn.Module):
+
+    def __init__(self):
+        super(Convention, self).__init__()
+
+        self.timestep = 0.05
+        self.n_steps = 21
+        self.A = torch.tensor([[1, self.timestep], [0, 1]])
+        self.B = torch.tensor([[0], [self.timestep]])
+        self.Qcost = torch.tensor([[10.0, 0.0], [0.0, 1.0]])
+        self.Rcost = 1.0
+        self.n_tasks = 5
+        self.omega = np.zeros((2,self.n_tasks))
+        self.omega[0,:] = np.linspace(0.2,1,self.n_tasks)
+        self.s_0 = torch.tensor([[0.0],[0.0]])
+
+        self.alpha = None
+        self.fch1 = nn.Linear(4,4)
+        self.fch2 = nn.Linear(4,1)
+        self.fcr1 = nn.Linear(3,3)
+        self.fcr2 = nn.Linear(3,1)
+
+        self.cost = []
+        self.diverg = []
+
+    def dynamics(self, s_star, s):
+        s_star_list = [s_star[0,0], s_star[1,0]]
+        s_list = [s[0,0], s[1,0]]
+        z = self.human(s_star_list, s_list)
+        a = self.robot(s_list, z)
+        s_next = self.A@s + self.B*a
+        return s_next, z
+
+    def human_initial(self, s_star, s):
+        e = torch.tensor([[s_star[0,0]-s[0,0]],[s_star[1,0]-s[1,0]]])
+        z = torch.tensor([[1.0, 0.1]]) @ e
+        return z[0]
+
+    def human(self, s_star, s):
+        x = torch.FloatTensor(s_star + s)
+        h1 = torch.tanh(self.fch1(x))
+        return self.fch2(h1)
+
+    def robot(self, s, z):
+        x = torch.cat((torch.FloatTensor(s), z), dim=0)
+        h1 = torch.tanh(self.fcr1(x))
+        return self.fcr2(h1)
+
+    def cost_J(self, s_star, s_0):
+        s = copy.deepcopy(s_0)
+        effort, error, normal = 0, 0, 0
+        for idx in range(self.n_steps - 1):
+            e = s_star - s
+            zt = self.human_initial(s_star, s)
+            s, z = self.dynamics(s_star, s)
+            error += torch.transpose(e, 0, 1) @ self.Qcost @ e
+            effort += self.Rcost * z**2
+            normal += (zt - z)**2
+        e = s_star - s
+        error += torch.transpose(e, 0, 1) @ self.Qcost @ e
+        return error, effort, normal
+
+    def cost_Q(self):
+        expected_cost, expected_diverg = 0.0, 0.0
+        for idx in range(self.n_tasks):
+            s_star = torch.tensor([[self.omega[0,idx]],[self.omega[1,idx]]])
+            error, effort, normal = self.cost_J(s_star, self.s_0)
+            expected_cost += (error + effort) / self.n_tasks
+            expected_diverg += normal / self.n_tasks
+        expected_total = expected_cost + self.alpha * expected_diverg
+        self.cost.append(expected_cost.item())
+        self.diverg.append(expected_diverg.item())
+        return expected_total
+
+    def rollout(self, s_star):
+        s = copy.deepcopy(self.s_0)
+        xi_s, xi_z = np.zeros((2,self.n_steps)), np.zeros((1,self.n_steps-1))
+        for idx in range(self.n_steps - 1):
+            xi_s[0,idx] = s[0,0]
+            xi_s[1,idx] = s[1,0]
+            s, z = self.dynamics(s_star, s)
+            xi_z[0,idx] = z[0]
+        xi_s[0,self.n_steps-1] = s[0,0]
+        xi_s[1,self.n_steps-1] = s[1,0]
+        return xi_s, xi_z
+
+    def rollout_J(self, s_star, s_0):
+        s = copy.deepcopy(s_0)
+        xi_s, xi_z = np.zeros((2,self.n_steps)), np.zeros((1,self.n_steps-1))
+        for idx in range(self.n_steps - 1):
+            xi_s[0,idx] = s[0,0]
+            xi_s[1,idx] = s[1,0]
+            s, z = self.dynamics(s_star, s)
+            xi_z[0,idx] = z[0]
+        xi_s[0,self.n_steps-1] = s[0,0]
+        xi_s[1,self.n_steps-1] = s[1,0]
+        return xi_s, xi_z
+
+    def traj_Q(self):
+        time = np.linspace(0,1,self.n_steps)
+        for idx in range(self.n_tasks):
+            s_star = torch.tensor([[self.omega[0,idx]],[self.omega[1,idx]]])
+            xi_s, xi_z = self.rollout_J(s_star, self.s_0)
+            plt.plot(1,s_star[0,0], 'kx')
+            plt.plot(time, xi_s[0,:])
+            plt.plot(time[:-1], xi_z[0,:])
+        plt.show()
---
+++ april-21/test-world.py
@@ -0,0 +1,152 @@
+import pygame
+import sys
+import os
+import math
+import numpy as np
+import time
+import random
+import pickle
+import copy
+import torch
+from models import Convention
+
+
+class Joystick(object):
+
+    def __init__(self):
+        self.gamepad = pygame.joystick.Joystick(0)
+        self.gamepad.init()
+        self.DEADBAND = 0.1
+
+    def input(self):
+        pygame.event.get()
+        z = self.gamepad.get_axis(0)
+        if abs(z) < self.DEADBAND:
+            z = 0.0
+        e_stop = self.gamepad.get_button(0)
+        return z, e_stop
+
+
+class Goal(pygame.sprite.Sprite):
+
+    def __init__(self, x):
+
+        # create sprite
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25,25))
+        self.image.fill((128, 128, 128))
+        self.rect = self.image.get_rect()
+
+        # initial conditions
+        self.x = x
+        self.rect.x = (self.x * 1000) + 200 - self.rect.size[0] / 2
+        self.rect.y = 100 - self.rect.size[1] / 2
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+
+        # create sprite
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+        # initial conditions
+        self.x = 0
+        self.xdot = 0.0
+        self.rect.x = (self.x * 1000) + 200 - self.rect.size[0] / 2
+        self.rect.y = 100 - self.rect.size[1] / 2
+
+        # current inputs
+        self.a = 0.0
+        self.z = 0.0
+        self.timestep = 0.05
+
+    def update(self, a):
+
+        # get the table
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+        # integrate to get new state
+        self.x = self.x + self.xdot * self.timestep
+        self.xdot = self.xdot + float(a) * self.timestep
+
+        # update the table position
+        self.rect.x = (self.x * 1000) + 200 - self.rect.size[0] / 2
+
+
+class Model(object):
+
+    def __init__(self):
+        self.model = Convention()
+        model_dict = torch.load('models/influence_a1.pt', map_location='cpu')
+        self.model.load_state_dict(model_dict)
+        self.model.eval
+
+    def human_initial(self, s_star, s):
+        return self.model.human_initial(s_star, s)
+
+    def human(self, s_star, s):
+        return self.model.human(s_star, s)
+
+    def robot(self, s, z):
+        return self.model.robot(s, z)
+
+
+def main():
+
+    clock = pygame.time.Clock()
+    pygame.init()
+    fps = 20
+
+    world = pygame.display.set_mode([1400,200])
+
+    player = Player()
+    joystick = Joystick()
+    model = Model()
+
+    g1 = Goal(0.2)
+    g2 = Goal(0.4)
+    g3 = Goal(0.6)
+    g4 = Goal(0.8)
+    g5 = Goal(1.0)
+
+    sprite_list = pygame.sprite.Group()
+    sprite_list.add(player)
+    sprite_list.add(g1)
+    sprite_list.add(g2)
+    sprite_list.add(g3)
+    sprite_list.add(g4)
+    sprite_list.add(g5)
+
+    while True:
+
+        # human
+        z, e_stop = joystick.input()
+        z = torch.FloatTensor([z])
+        if e_stop:
+          pygame.quit(); sys.exit()
+
+        s_star = [0.8, 0.0]
+        s = [player.x, player.xdot]
+        print(s)
+
+        z = model.human(s_star, s)
+
+        # robot
+        a = model.robot(s, z)
+
+        # dynamics
+        player.update(a)
+
+        # animate
+        world.fill((0,0,0))
+        sprite_list.draw(world)
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+if __name__ == "__main__":
+    main()
